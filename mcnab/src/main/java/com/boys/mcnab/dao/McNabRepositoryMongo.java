00b14db37bb083685daa0cabedfbbaf048499d78
Jake Toan <jaketoan@hotmail.co.uk>
2021-12-04 16:06:34 +0000

    Fixing beans

--- mcnab/src/main/java/com/boys/mcnab/dao/McNabRepositoryMongo.java
+++ mcnab/src/main/java/com/boys/mcnab/dao/McNabRepositoryMongo.java
@@ -1,10 +1,10 @@
 package com.boys.mcnab.dao;
 
-import com.boys.mcnab.model.McNab;
+import com.boys.mcnab.model.McNabMongo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface McNabRepositoryMongo extends MongoRepository<McNab, String>
+public interface McNabRepositoryMongo extends MongoRepository<McNabMongo, String>
 {
 }
--- mcnab/src/main/java/com/boys/mcnab/dao/McNabRepositorySQL.java
+++ mcnab/src/main/java/com/boys/mcnab/dao/McNabRepositorySQL.java
@@ -1,10 +1,10 @@
 package com.boys.mcnab.dao;
 
-import com.boys.mcnab.model.McNab;
+import com.boys.mcnab.model.McNabSQL;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface McNabRepositorySQL extends JpaRepository<McNab, String>
+public interface McNabRepositorySQL extends JpaRepository<McNabSQL, String>
 {
 }
